62a8f1e6fe6a5714381de07548bb0e6cac47c31b
SHANTOOS <shantunusharma2@gmail.com>
2024-06-26 16:32:40 +0600

    Update DDOS_ATTACK_Tool.py

--- DDOS_ATTACK_Tool.py
+++ DDOS_ATTACK_Tool.py
@@ -101,7 +101,7 @@ def httpcall(url):
 	except urllib2.HTTPError, e:
 			#print e.code
 			set_flag(1)
-			print 'One of the most powerfull DDOS attack tool (responce code : 500)'
+			print 'Responceing... code(500)'
 			code=500
 	except urllib2.URLError, e:
 			#print e.reason
